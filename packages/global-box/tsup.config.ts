7fa8c332d3b30062a365cdf4ea5e36124608c2ca
Krist Wongsuphasawat <krist.wongz@gmail.com>
2024-06-01 01:17:21 -0700

    chore: update config

--- packages/global-box/tsup.config.ts
+++ packages/global-box/tsup.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig, type Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
-  entry: ['./src/**/*.{ts,tsx}'],
+  entry: ['./src/index.{ts,tsx}'],
   format: ['cjs', 'esm'],
   dts: true,
-  external: ['react'],
   banner: {
     js: "'use client'",
   },
