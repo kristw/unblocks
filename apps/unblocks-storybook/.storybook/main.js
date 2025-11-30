7b4fa1273634f3632fee0e2da7ceb5e9e3b6c7e5
Krist Wongsuphasawat <krist.wongz@gmail.com>
2025-11-30 15:35:53 -0800

    chore: upgrade storybook

--- apps/unblocks-storybook/.storybook/main.js
+++ apps/unblocks-storybook/.storybook/main.js
@@ -1,11 +1,6 @@
 module.exports = {
   stories: ['../src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
-
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
 
   framework: {
     name: '@storybook/nextjs',
@@ -20,9 +15,5 @@ module.exports = {
     // So we can use @/ to start from the src folder
     // config.resolve.alias['@'] = path.resolve(__dirname, '../src');
     return config;
-  },
-
-  docs: {
-    autodocs: true,
-  },
+  }
 };
